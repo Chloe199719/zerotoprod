7334d537973b63a2795c35dfda5cf9fbc4ce02df
Chloe Pratas <chloevision97@gmail.com>
2023-07-24 03:52:22 +0200

    init Project

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(index)
+            .service(greet)
+            .service(health_check)
+           
+    })
+    .bind(("127.0.0.1", 3000))?
+    .run()
+    .await
+}
+
+
+#[get("/greet")]
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[get("/greet/{name}")]
+async fn greet(path: web::Path<(String,)>) -> impl Responder {
+    HttpResponse::Ok().body(format!("Hello {}!", path.0))
+}
+
+
+#[get("/health-check")]
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok().body("I am alive!")
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use actix_web::test;
+
+    #[actix_web::test]
+    async fn test_health_check() {
+        let app = test::init_service(App::new().service(health_check)).await;
+        let req = test::TestRequest::get().uri("/health-check").to_request();
+        let resp = test::call_service(&app,req).await;
+        assert!(resp.status().is_success());
+    }
 }
